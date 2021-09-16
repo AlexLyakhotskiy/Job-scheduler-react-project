5a15a7b5549881e3e6729a553f994520f9f6471a
Alexander Lyakhotskiy <alexlyakhotskiy@gmail.com>
2021-09-16 22:15:30 +0300

    changed routes address

--- src/pages/SprintsPage/SprintsPage.js
+++ src/pages/SprintsPage/SprintsPage.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import { Link, useLocation, useRouteMatch, useParams } from 'react-router-dom';
+import { Link, useRouteMatch, useParams } from 'react-router-dom';
 import { routes } from '../../routes/routes';
 
 export default function SprintsPage() {
   const { projectId } = useParams();
   const { url } = useRouteMatch();
-  const location = useLocation();
 
   console.log('projectId: ', projectId);
   return (
     <>
       <h1>SprintsPage</h1>
-      <Link
-        to={{
-          pathname: `${url}/sprints/555`,
-          state: { from: location },
-        }}
-      >
-        к таскам
-      </Link>
+      <Link to={`${url}/555`}>к таскам</Link>
       <div>
         <Link to={routes.projects}>назад</Link>
       </div>
