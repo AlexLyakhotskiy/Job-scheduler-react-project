7138f97b7790ea856bd6405f59d884c3dfe3127e
mandarinka99 <alina.oksentiuk@gmail.com>
2021-09-17 11:34:02 +0300

    fix

--- src/components/Tasks/TaskItem/CreateTaskForm/CreateTaskForm.js
+++ src/components/Tasks/TaskItem/CreateTaskForm/CreateTaskForm.js
@@ -1,14 +1,51 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addTask } from '../../../../redux/tasks/tasks-operations';
+
+const initialState = {
+  title: '',
+  hoursPlanned: '',
+};
+
 const CreateTaskForm = () => {
+  const [task, setTask] = useState({ ...initialState });
+  const dispatch = useDispatch();
+
+  const onHandleChange = e => {
+    const { name, value } = e.target;
+    setTask({ ...task, [name]: value });
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    dispatch(addTask({ title: task.title, hoursPlanned: task.hours }));
+    setTask({
+      title: '',
+      hoursPlanned: '',
+    });
+  };
   return (
     <div>
-      <form>
+      <form onSubmit={onSubmit}>
         <label>
           Назва задачі
-          <input type="text" name="name" value="" required onChange="" />
+          <input
+            type="text"
+            name="title"
+            value={task.title}
+            onChange={onHandleChange}
+            required
+          />
         </label>
         <label>
           Заплановано годин
-          <input type="number" name="number" value="" required onChange="" />
+          <input
+            type="number"
+            name="hoursPlanned"
+            value={task.hoursPlanned}
+            onChange={onHandleChange}
+            required
+          />
         </label>
         <button type="submit">Готово</button>
       </form>
