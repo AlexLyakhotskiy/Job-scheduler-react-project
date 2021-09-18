95a0a770ac442c6714ff3e3d4ad77f8d780a8985
Alexander Lyakhotskiy <alexlyakhotskiy@gmail.com>
2021-09-19 01:11:49 +0300

    added component input

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,32 @@
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import React from 'react';
 
 import Main from './Main/Main';
 
+import Input from './Input/Input';
+import Button from './Button/Button';
+
+const validationSchema = Yup.object().shape({
+  nameYourInput: Yup.string()
+    .min(2, 'Занадто коротка назва, мін 2 символа')
+    .max(16, 'Занадто довга назва, макс 16 символів')
+    .required("Поле обов'язкове"),
+});
+
 function App() {
+  const formik = useFormik({
+    initialValues: { nameYourInput: '' },
+    validationSchema,
+    onSubmit: values => console.log(values),
+  });
   return (
     <>
       <Main />
+      <form onSubmit={formik.handleSubmit}>
+        <Input formik={formik} name="nameYourInput" />
+        <Button />
+      </form>
     </>
   );
 }
