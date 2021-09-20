3e5b3a622e5fe97165aabcea2174f83d582ed881
IgnatevD <ignatevds@gmail.com>
2021-09-21 01:32:59 +0300

    Удален лишний код

--- src/components/Sprints/SpintBtAddProject/SpintBtAddProject.jsx
+++ src/components/Sprints/SpintBtAddProject/SpintBtAddProject.jsx
@@ -1,42 +1,13 @@
 import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { addProjects } from '../../../redux/projects/projectOperations';
 import AddProjectsForm from '../../AddProjectsForm/AddProjectsForm';
 import IconBtn from '../../IconBtn/IconBtn';
 import Modal from '../../Modal';
 import s from './SpintBtAddProject.module.scss';
 
 const SpintBtAddProject = () => {
-  const dispatch = useDispatch();
   const [showModal, setShowModal] = useState(false);
   const toggleModal = () => {
     setShowModal(state => !state);
-  };
-
-  const [titleInput, setTitleInput] = useState('');
-  const [descriptionInput, setDescriptionInput] = useState('');
-
-  const handleChange = event => {
-    const { name, value } = event.target;
-
-    switch (name) {
-      case 'title':
-        setTitleInput(value);
-        break;
-      case 'description':
-        setDescriptionInput(value);
-        break;
-      default:
-        return;
-    }
-  };
-
-  const handleSubmit = event => {
-    event.preventDefault();
-    dispatch(addProjects({ title: titleInput, description: descriptionInput }));
-    setTitleInput('');
-    setDescriptionInput('');
-    toggleModal();
   };
 
   return (
