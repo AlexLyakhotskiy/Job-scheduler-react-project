65e1c61b0577c6818d56bfcfe9180ec65157a936
Masha-Shytykova <mashtakova.mash@gmail.com>
2021-09-20 11:56:40 +0300

    project_add_form

--- src/components/AddProjectsForm/AddProjectsForm.js
+++ src/components/AddProjectsForm/AddProjectsForm.js
@@ -1,22 +1,52 @@
-import { Formik, Form, Field } from 'formik';
+//import { Formik, Form, Field } from 'formik';
+
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import Input from '../Input/Input.jsx';
+import Button from '../Button/Button.jsx';
+import CancelBtn from '../CancelBtn/CancelBtn.jsx';
 import css from './AddProjectsForm.module.scss';
 
+const validationSchema = Yup.object().shape({
+  nameYourInput: Yup.string()
+    .min(2, 'Занадто коротка назва, мін 2 символа!')
+    .max(16, 'Занадто довга назва, макс 16 символів!')
+    .required("Поле обов'язкове!"),
+});
+
 export default function AddProjectsForm() {
+  const formik = useFormik({
+    initialValues: { nameYourInput: '' },
+    validationSchema,
+    onSubmit: values => {
+      console.log(values);
+    },
+  });
+
   return (
-    <Formik>
-      <Form className={css.formProject} autoComplete="off">
-        <label className={css.nameLabel}>
-          <Field name="title" placeholder=" " className={css.nameInp} />
-          <span className={css.nameLabelText}>Назва проекту</span>
-        </label>
+    <>
+      <form onSubmit={formik.handleSubmit}>
+        <Input formik={formik} name="nameYourInput" label="Назва проекту" />
+        <Input formik={formik} name="nameYourInput" label="Опис" />
+        <Button />
+        <CancelBtn />
+      </form>
+    </>
 
-        <label className={css.descrLabel}>
-          <Field name="description" placeholder=" " className={css.descrInp} />
-          <span className={css.descrLabelText}>Опис</span>
-        </label>
+    // <Formik>
+    //   <Form className={css.formProject} autoComplete="off">
+    //     <label className={css.nameLabel}>
+    //       <Field name="title" placeholder=" " className={css.nameInp} />
+    //       <span className={css.nameLabelText}>Назва проекту</span>
+    //     </label>
 
-        <button type="submit">Готово</button>
-      </Form>
-    </Formik>
+    //     <label className={css.descrLabel}>
+    //       <Field name="description" placeholder=" " className={css.descrInp} />
+    //       <span className={css.descrLabelText}>Опис</span>
+    //     </label>
+    //     <Button />
+    //     <CancelBtn />
+    //   </Form>
+    // </Formik>
   );
 }
