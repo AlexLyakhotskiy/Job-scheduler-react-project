99abdf4a33c8f7fee84b59525c5d62c4d98c8248
vip-master <1998.illia.z@gmail.com>
2021-09-23 03:13:13 +0300

    draft burger

--- src/components/App.js
+++ src/components/App.js
@@ -7,12 +7,34 @@ import Main from './Main/Main';
 import Footer from './Footer/Footer';
 
 import { resetUser } from '../redux/auth/auth-operations';
-import { getIsResetingUser } from '../redux/auth/auth-selectors';
+import { getIsResetingUser} from '../redux/auth/auth-selectors';
+import { getTheme } from '../redux/userSettings/userSettingsSelectors.js';
+import { changeTheme } from '../redux/userSettings/userSettingsActions.js';
+
+export const light = 'light';
+export const dark = 'dark';
 
 function App() {
   const history = useHistory();
   const isResetingUser = useSelector(getIsResetingUser);
+  const theme = useSelector(getTheme);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    switch (theme) {
+      case light:
+        document.body.classList.add('light-theme');
+        document.body.classList.remove('dark-theme');
+        break;
+      case dark:
+        document.body.classList.add('dark-theme');
+        document.body.classList.remove('light-theme');
+        break;
+      default:
+        dispatch(changeTheme(light));
+    }
+  }, [theme]);
+
 
   useEffect(() => {
     const emptyPath = history.location.pathname === '/';
