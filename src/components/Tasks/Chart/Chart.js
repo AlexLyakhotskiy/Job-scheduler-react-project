eb0ffbbb11bb22efdb8e52949da0f41211fb6ebd
mandarinka99 <alina.oksentiuk@gmail.com>
2021-09-23 05:13:31 +0300

    searchInput, select, styles

--- src/components/Tasks/Chart/Chart.js
+++ src/components/Tasks/Chart/Chart.js
@@ -17,15 +17,12 @@ const Chart = ({ tasks }) => {
     return sortedDaysArr;
   };
 
-  console.log(`tasks`, tasks);
-
   const hoursPlanedSum = tasks.reduce(
     (acc, task) => acc + Number(task.hoursPlanned),
     0,
   );
   // планир. затраченные чвсы за день
 
-  // const getPlanedHoursArr = () => {
   const planedHoursArr = []; //[hoursPlanedSum]
   let hoursScheduled = hoursPlanedSum;
   for (let i = 0; i < tasks[0].hoursWastedPerDay.length; i += 1) {
@@ -33,8 +30,6 @@ const Chart = ({ tasks }) => {
       hoursScheduled - hoursPlanedSum / tasks[0].hoursWastedPerDay.length;
     planedHoursArr.push(hoursScheduled);
   }
-  //   return planedHoursArr;
-  // };
 
   // const getHoursWastedArr = () => {
   const daysWastedHours = {};
@@ -56,120 +51,114 @@ const Chart = ({ tasks }) => {
     daysWastedHoursArr.push(hoursFact);
   });
 
-  //   return daysWastedHoursArr;
-  // };
-
   // возвращает масив для запланировыныхтрудозатрат
+  // const mainHoursArr = [];
 
+  // planedHoursArr.forEach((item, index) => {
+  //     if (daysWastedHoursArr[index] <= item) {
+  //       mainHoursArr.push(daysWastedHoursArr[index])
 
-  const mainHoursArr = []; // масив запланированых + %
+  //     } else {
+  //       mainHoursArr.push(Number(item) / Number(daysWastedHoursArr[index]) + Number(item));
+  //     }
+  //   }
 
-  
-  const sfdsf = () => {
-    planedHoursArr.forEach((item, index) => {
-      if (Number(daysWastedHoursArr[index]) <= Number(item)) {
-        mainHoursArr.push(daysWastedHoursArr[index]);
-      } else {
-        mainHoursArr.push(Number(item) / Number(daysWastedHoursArr[index]) + Number(item));
-      }
-  }
+  // const updateSum = mainHoursArr.reduce(
+  //   (acc, hourItem) => acc + Number(hourItem),
+  //   0,
+  // );
+  // const redLine = [];
 
-      // const updateSum = mainHoursArr.reduce(
-      //   (acc, hourItem) => acc + Number(hourItem), 0);
-      // const redLine = [];
+  // mainHoursArr.forEach(item => {
+  //   redLine.push(updateSum - item / item.length);
+  // });
 
-      // mainHoursArr.forEach(item => {
-      //   redLine.push(updateSum - item / item.length);
-      // });
-   
-  
-
-    const options = {
-      scales: {
-        yAxis: {
-          min: 0,
-        },
+  const options = {
+    scales: {
+      yAxis: {
+        min: 0,
       },
-    };
-
-    const data = {
-      labels: getperiodArr(),
-      options: {
-        scales: {
-          xAxes: [
-            {
-              ticks: {
-                beginAtZero: true,
-                max: 1000,
-                min: 0,
-              },
-            },
-          ],
-          yAxes: [
-            {
-              ticks: {
-                beginAtZero: false,
-                max: 8,
-                min: -3,
-              },
-            },
-          ],
-        },
-      },
-      datasets: [
-        {
-          label: curLanguage.tasks.chart.labelone,
-          data: [updateSum, ...planedHoursArr],
-          fill: false,
-          lineTension: 0.1,
-          backgroundColor: 'rgba(255, 0, 81, 1)',
-          borderColor: 'rgba(255, 0, 81, 1)',
-          borderCapStyle: 'butt',
-          borderDash: [],
-          borderDashOffset: 1,
-          borderJoinStyle: 'miter',
-          pointBorderColor: 'rgba(255, 0, 81, 1)',
-          pointBackgroundColor: '#fff',
-          pointBorderWidth: 5,
-          pointHoverRadius: 10,
-          pointHoverBackgroundColor: 'rgba(255, 0, 81, 1)',
-          pointHoverBorderColor: 'rgba(255, 0, 81, 1)',
-          pointHoverBorderWidth: 2,
-          pointRadius: 3,
-          pointHitRadius: 10,
-        },
-        {
-          label: curLanguage.tasks.chart.labeltwo,
-          data: [hoursPlanedSum, ...daysWastedHoursArr],
-          fill: false,
-          lineTension: 0.5,
-          backgroundColor: 'rgba(0, 0, 123, 1)',
-          borderColor: 'rgba(0, 0, 123, 1)',
-          borderCapStyle: 'butt',
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: 'miter',
-          pointBorderColor: 'rgba(0, 0, 123, 1)',
-          pointBackgroundColor: '#fff',
-          pointBorderWidth: 5,
-          pointHoverRadius: 10,
-          pointHoverBackgroundColor: 'rgba(0, 0, 123, 1)',
-          pointHoverBorderColor: 'rgba(0, 0, 123, 1)',
-          pointHoverBorderWidth: 2,
-          pointRadius: 3,
-          pointHitRadius: 10,
-        },
-      ],
-    };
-
-    return (
-      <div className={s.scrol}>
-        <div className={s.charContainer}>
-          <h2 className={s.charTitle}>Burndown Chart (Calendar team)</h2>
-          <Line data={data} options={options} />
-        </div>
-      </div>
-    );
+    },
   };
+
+  const data = {
+    labels: getperiodArr(),
+    options: {
+      scales: {
+        xAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+              max: 1000,
+              min: 0,
+            },
+          },
+        ],
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: false,
+              max: 8,
+              min: -3,
+            },
+          },
+        ],
+      },
+    },
+    datasets: [
+      {
+        label: curLanguage.tasks.chart.labelone,
+        data: [hoursPlanedSum, ...planedHoursArr],
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(255, 0, 81, 1)',
+        borderColor: 'rgba(255, 0, 81, 1)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 1,
+        borderJoinStyle: 'miter',
+        pointBorderColor: 'rgba(255, 0, 81, 1)',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 5,
+        pointHoverRadius: 10,
+        pointHoverBackgroundColor: 'rgba(255, 0, 81, 1)',
+        pointHoverBorderColor: 'rgba(255, 0, 81, 1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 3,
+        pointHitRadius: 10,
+      },
+      {
+        label: curLanguage.tasks.chart.labeltwo,
+        data: [hoursPlanedSum, ...daysWastedHoursArr],
+        fill: false,
+        lineTension: 0.5,
+        backgroundColor: 'rgba(0, 0, 123, 1)',
+        borderColor: 'rgba(0, 0, 123, 1)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: 'rgba(0, 0, 123, 1)',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 5,
+        pointHoverRadius: 10,
+        pointHoverBackgroundColor: 'rgba(0, 0, 123, 1)',
+        pointHoverBorderColor: 'rgba(0, 0, 123, 1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 3,
+        pointHitRadius: 10,
+      },
+    ],
+  };
+
+  return (
+    <div className={s.scrol}>
+      <div className={s.charContainer}>
+        <h2 className={s.charTitle}>Burndown Chart (Calendar team)</h2>
+        <Line data={data} options={options} />
+      </div>
+    </div>
+  );
+};
 
 export default Chart;
